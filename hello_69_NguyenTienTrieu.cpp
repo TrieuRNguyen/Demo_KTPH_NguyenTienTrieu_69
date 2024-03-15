15da21cc78348881af3ce5e6a331c8db1ac12866
Trieu Nguyen <trieutttnguyen@gmail.com>
2024-03-15 17:20:00 +0700

    Thay doi de tao conflict tren hello_69_NguyenTienTrieu.cpp

---
+++ hello_69_NguyenTienTrieu.cpp
@@ -0,0 +1,26 @@
+<<<<<<<<<<<test
+==========
+
+<<<<<<<<<<main
+
+
+// C++ program to display "Hello World"
+
+// header file for input output functions
+#include <iostream>
+using namespace std;
+
+// Main() funtion: where the execution of
+// program begins
+int main()
+{
+	//Prints hello world
+	cout<<"Hello World";
+	cout<<"Thay Doi Tren Branch test" << endl;
+	<<<<<<<<<<<test
+	==========
+
+	cout<<"Thay Doi Tren Branch main" << endl;
+	<<<<<<<<<<main
+    return 0;
+}
